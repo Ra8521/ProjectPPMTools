52e742f911a1c6b2565cb00689162ae5e441f04b
Ra8521 <taraon71@gmail.com>
2021-05-09 16:32:21 +0530

    Dashboard component added

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,26 +1,19 @@
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import Dashboard from './components/Dashboard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1> Welcome to PPM Tools</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+      <Dashboard />
+      <Dashboard />
+      <Dashboard />
+      </div>
+    );
+  }
 }
+
 
 export default App;

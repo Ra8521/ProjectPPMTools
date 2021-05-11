8acf7ad1050cbb4c941c80918714819d8c85dc9c
Ra8521 <taraon71@gmail.com>
2021-05-11 17:49:37 +0530

    bootstrap added

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import ProjectItem from './Project/ProjectItem';
-
+import "bootstrap/dist/css/bootstrap.min.css";
 class Dashboard extends Component {
     render() {
         return (
             <div className="Dashboard">
-            <h1> Welcome to dashboard </h1>
+            <h1 className="alert alert-warning"> Welcome to dashboard </h1>
             <ProjectItem/>
             <ProjectItem/>
             <ProjectItem/>

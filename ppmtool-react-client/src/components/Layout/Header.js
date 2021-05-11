e799c0741a05bf57aed5af6ed77ac49aa559879d
Ra8521 <taraon71@gmail.com>
2021-05-11 17:23:18 +0530

    projectItem component added

---
+++ ppmtool-react-client/src/components/Layout/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class Header extends Component {
+    render() {
+        return (
+            <div class="Header">
+                <h1>Navbar</h1>
+            </div>
+        );
+    }
+}
+
+export default Header
---
+++ ppmtool-react-client/src/components/Project/ProjectItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+ class ProjectItem extends Component {
+    render() {
+        return (
+            <h1>Welcome to ProjectItem</h1>
+        );
+    }
+}
+
+export default ProjectItem

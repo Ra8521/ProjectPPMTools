3fc87dcf2aa685d2f8db999eb7c4107a616ab28d
Ra8521 <taraon71@gmail.com>
2021-05-11 21:11:16 +0530

    develop component

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -4,12 +4,23 @@ import "bootstrap/dist/css/bootstrap.min.css";
 class Dashboard extends Component {
     render() {
         return (
-            <div className="Dashboard">
-            <h1 className="alert alert-warning"> Welcome to dashboard </h1>
-            <ProjectItem/>
-            <ProjectItem/>
-            <ProjectItem/>
+            <div className="projects">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1 className="display-4 text-center">Projects</h1>
+                        <br />
+                        <a href="ProjectForm.html" className="btn btn-lg btn-info">
+                            Create a Project
+                        </a>
+                        <br />
+                        <hr />
+                    <ProjectItem/>
+                    </div>
+                </div>
             </div>
+        </div>
+    
         );
     }
 }

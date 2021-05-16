a3bbb74a0b648b493edcb74790115cbfaecef77a
Ra8521 <taraon71@gmail.com>
2021-05-16 15:10:04 +0530

    fixing bug related to updating of project

--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -6,8 +6,12 @@ export const createProject = (project, history) => async dispatch => {
   try {
     const res = await axios.post("http://localhost:8080/api/project", project);
     history.push("/dashboard");
+    dispatch({
+      type: GET_ERRORS,
+      payload: {}
+    });
   } catch (err) {
-    console.log(err);
+    //console.log(err);
     dispatch({
       type: GET_ERRORS,
       payload: err.response.data

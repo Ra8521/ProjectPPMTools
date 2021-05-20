63e2820bd089ca60d86f3a026ba16c7ae5c8ddd4
Ra8521 <taraon71@gmail.com>
2021-05-20 08:39:28 +0530

    action type and reducer created for project task

--- ppmtool-react-client/src/actions/types.js
+++ ppmtool-react-client/src/actions/types.js
@@ -4,3 +4,9 @@ export const GET_ERRORS = "GET_ERRORS";
 export const GET_PROJECTS = "GET_PROJECTS";
 export const GET_PROJECT = "GET_PROJECT";
 export const DELETE_PROJECT = "DELETE_PROJECT";
+
+//TYPES FOR   BACKLOG
+export const GET_BACKLOG = "GET_BACKLOG";
+export const GET_PROJECT_TASK = "GET_PROJECT_TASK";
+export const DELETE_PROJECT_TASK = "DELETE_PROJECT_TASK";
+

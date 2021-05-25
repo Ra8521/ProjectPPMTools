a4f2ed8fc5ac39bfc92f09074055d7ad6fcf35fd
Ra8521 <taraon71@gmail.com>
2021-05-25 21:27:23 +0530

    create control over registeration component

--- ppmtool-react-client/src/components/UserManagement/Register.js
+++ ppmtool-react-client/src/components/UserManagement/Register.js
@@ -1,6 +1,35 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import { createNewUser } from "../../actions/securityActions";
+import {PropTypes} from "prop-types";
+import { connect } from "react-redux";
+import classnames from "classnames";
  class Register extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      username: "",
+      fullName: "",
+      password: "",
+      confirmPassword: "",
+      errors: {}
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onSubmit(e) {
+    e.preventDefault();
+    const newUser = {
+      username: this.state.username,
+      fullName: this.state.fullName,
+      password: this.state.password,
+      confirmPassword: this.state.confirmPassword
+    };
+    this.props.createNewUser(newUser, this.props.history);
+  }
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
     render() {
         return (
       <div className="register">
@@ -9,22 +38,25 @@ import React, { Component } from 'react'
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Sign Up</h1>
               <p className="lead text-center">Create your Account</p>
-              <form action="create-profile.html">
+              <form onSubmit={this.onSubmit}>
                 <div className="form-group">
                   <input
                     type="text"
                     className="form-control form-control-lg"
-                    placeholder="Name"
-                    name="name"
-                    required
+                    placeholder="Full Name"
+                    name="fullName"
+                    value={this.state.fullName}
+                    onChange={this.onChange}
                   />
                 </div>
                 <div className="form-group">
                   <input
                     type="email"
                     className="form-control form-control-lg"
-                    placeholder="Email Address"
-                    name="email"
+                    placeholder="Email Address(User name)"
+                    name="username"
+                    value={this.state.username}
+                    onChange={this.onChange}
                   />
                 </div>
                 <div className="form-group">
@@ -33,6 +65,8 @@ import React, { Component } from 'react'
                     className="form-control form-control-lg"
                     placeholder="Password"
                     name="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
                   />
                 </div>
                 <div className="form-group">
@@ -40,7 +74,9 @@ import React, { Component } from 'react'
                     type="password"
                     className="form-control form-control-lg"
                     placeholder="Confirm Password"
-                    name="password2"
+                    name="confirmPassword"
+                    value={this.state.confirmPassword}
+                    onChange={this.onChange}
                   />
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
@@ -52,4 +88,11 @@ import React, { Component } from 'react'
     );
     }
 }
-export default Register
+Register.propTypes = {
+  createNewUser : PropTypes.func.isRequired,
+  errors : PropTypes.object.isRequired
+}
+const mapStatetoProps = state => ({
+  errors: state.errors
+});    
+export default connect(mapStatetoProps,{createNewUser})(Register);
